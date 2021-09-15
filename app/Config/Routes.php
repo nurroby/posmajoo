dc83ea2abc16a61b9202993bc8aabf58bdaddfdd
Nurroby Wahyu Saputra <inbox.ibor@gmail.com>
2021-09-15 15:11:53 +0700

    🚧 add routes for admin, user, api

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,6 +32,33 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/login', 'Home::login');
+$routes->get('/register', 'Home::register');
+
+$routes->group('admin', function ($routes) {
+    $routes->add('/', 'Admin\Home::index');
+    $routes->add('login', 'Admin\Home::login');
+    $routes->add('auth', 'Admin\Home::auth');
+    $routes->add('logout', 'Admin\Home::logout');
+    $routes->add('category', 'Admin\Category::index');
+    $routes->add('category/add', 'Admin\Category::create');
+    $routes->add('category/edit', 'Admin\Category::update');
+});
+
+$routes->group('users', function ($routes) {
+    $routes->add('/', 'User\Home::index');
+    $routes->add('logout', 'User\Home::index');
+});
+
+$routes->group('api', function ($routes) {
+    $routes->get('/', 'Api\Home::index');
+    $routes->get('admin/login', 'Api\Admin::login');
+    $routes->get('admin/logout', 'Api\Admin::logout');
+    $routes->get('category', 'Api\Category::index');
+        $routes->post('category', 'Api\Category::addCategory');
+        $routes->put('category', 'Api\Category::editCategory');
+        $routes->delete('category', 'Api\Category::deleteCategory');
+});
 
 /*
  * --------------------------------------------------------------------
