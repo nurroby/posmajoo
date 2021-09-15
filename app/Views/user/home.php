6f91777e84d700b4bcaca47c137d962ac984ccfd
Nurroby Wahyu Saputra <inbox.ibor@gmail.com>
2021-09-15 15:25:36 +0700

    🚧 init user home

---
+++ app/Views/user/home.php
@@ -0,0 +1,18 @@
+<?= $this->extend('layout/user_layout') ?>
+<?= $this->section('content') ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper majoo-bg">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container">
+            <div class="row mb-2">
+                <div class="col-sm-12">
+                    <h1 class="m-0"> Ini USER <small> - </small></h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+<!-- /.content-wrapper -->
+<?= $this->endSection() ?>
