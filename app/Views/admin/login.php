f081382604068c8a398480a72d424420ae28a7c4
Nurroby Wahyu Saputra <inbox.ibor@gmail.com>
2021-09-15 15:25:12 +0700

    🚧 admin login

---
+++ app/Views/admin/login.php
@@ -0,0 +1,61 @@
+<?= view('css/base_admin') ?>
+<body class="majoo-bg">
+    <!-- Content Wrapper. Contains page content -->
+    <div class="majoo-bg">
+        <div class="content-header">
+            <div class="container">
+                <div class="row mb-2">
+                    <div class="col-sm-12">
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+        <div class="content">
+            <div class="container-fluid d-flex justify-content-center">
+                    <!-- login-box -->
+                    <div class="login-box">
+                        <!-- /.login-logo -->
+                        <div class="card card-outline card-success">
+                            <div class="card-header text-center">
+                                <img src="https://majoo.id/assets/img/main-logo.png" alt="">
+                            </div>
+                            <div class="card-body">
+                                <p class="login-box-msg">Sign in to start your session</p>
+
+                                <form action="<?= base_url().'/admin/auth'?>" method="post">
+                                    <div class="input-group mb-3">
+                                        <input type="email" class="form-control" placeholder="Email" required>
+                                        <div class="input-group-append">
+                                            <div class="input-group-text">
+                                                <span class="fas fa-envelope"></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="input-group mb-3">
+                                        <input type="password" class="form-control" placeholder="Password" required>
+                                        <div class="input-group-append">
+                                            <div class="input-group-text">
+                                                <span class="fas fa-lock"></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row justify-content-end">
+                                        <!-- /.col -->
+                                        <div class="col-4">
+                                            <button type="submit" class="btn btn-primary btn-block rounded-full">Sign In</button>
+                                        </div>
+                                        <!-- /.col -->
+                                    </div>
+                                </form>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.login-box -->
+            </div>
+        </div>
+    </div>
+<?= view('js/base_admin') ?>
+</body>
