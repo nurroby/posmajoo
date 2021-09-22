93b70a0d13dfc3bcbe3c5d1693a19425658bb2a8
Nurroby Wahyu Saputra <inbox.ibor@gmail.com>
2021-09-22 16:07:48 +0700

    🚧 connect catalog w/ db

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,12 +1,22 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\ProductModel;
+use App\Models\CategoryModel;
 
 class Home extends BaseController
 {
     public function index()
     {
-        return view('catalog');
+        $_product   = new ProductModel(); 
+        $_category  = new CategoryModel();
+        $data=[
+            'category'=>$_category->findAll(),
+            'product'=>$_product->select('product.id, product.name, product.price, product.description, product.image, category.name as category, product.category_id')
+                                ->join('category','category.id = product.category_id','left')
+                                ->findAll(),
+        ];
+        return view('catalog',$data);
     }
     public function login()
     {
