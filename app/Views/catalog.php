618caaad4ec2e41cad87ca6b791a36e751497a17
Nurroby Wahyu Saputra <inbox.ibor@gmail.com>
2021-09-28 19:13:22 +0700

    🚧 update globally js on front

--- app/Views/catalog.php
+++ app/Views/catalog.php
@@ -301,6 +301,96 @@
 
     displayCart();
 
+    
+    function template(data) {
+        if ($(data.html).length === 0) {
+            return data.text;
+        }
+        return data.html;
+    }
+    $("#category_id").select2({
+        theme:'bootstrap4',
+        placeholder: 'Pilih salah satu',
+        ajax: {
+            url: '<?= base_url(); ?>/api/category',
+            dataType: 'json',
+            type: 'GET',
+            processResults: function(data) {
+                let myResults = [];
+                if(data.category.length > 0) {
+                    $.each(data.category, function(index, item) {
+                        myResults.push({
+                            id: item.id,
+                            text: item.name.toUpperCase()
+                        });
+                    });
+                }
+                return {
+                    results: myResults
+                };
+            },
+            cache: true
+        },
+        allowClear: true,
+        placeholder: 'Select at least one element',
+        templateResult: template,
+        templateSelection: template
+    });
+    
+    $(document).ready(function () {
+        $('#category').change(function(){
+            if($(this).val()=="category-0"){
+                $('.category').show(); return;
+            }else{
+                $('.category').hide();
+            }
+            $('.' + $(this).val()).show();
+        });
+    });
+
+    document.addEventListener('DOMContentLoaded',function(event){
+        // array with texts to type in typewriter
+        var dataText = [ " monitor your bussiness ", " scale up ", " growth together ", " start ! "];
+
+        // type one text in the typwriter
+        // keeps calling itself until the text is finished
+        function typeWriter(text, i, fnCallback) {
+        // chekc if text isn't finished yet
+        if (i < (text.length)) {
+            // add next character to h1
+            document.querySelector("h1").innerHTML = 'Lets<bold class="bg-maroon" style="border-radius:0 0 0 0;">' + text.substring(0, i+1) +'</bold> with <bold style="color:teal">Majoo</bold><span aria-hidden="true"></span>';
+
+            // wait for a while and call this function again for next character
+            setTimeout(function() {
+            typeWriter(text, i + 1, fnCallback)
+            }, 75);
+        }
+        // text finished, call callback if there is a callback function
+        else if (typeof fnCallback == 'function') {
+            // call callback after timeout
+            setTimeout(fnCallback, 900);
+        }
+        }
+        // start a typewriter animation for a text in the dataText array
+        function StartTextAnimation(i) {
+            if (typeof dataText[i] == 'undefined'){
+            setTimeout(function() {
+                StartTextAnimation(0);
+            }, 10000);
+            }
+            // check if dataText[i] exists
+        if (i < dataText[i].length) {
+            // text exists! start typewriter animation
+            typeWriter(dataText[i], 0, function(){
+            // after callback (and whole text has been animated), start next text
+            StartTextAnimation(i + 1);
+            });
+        }
+        }
+        // start the text animation
+        StartTextAnimation(0);
+    });
+
 </script>
 <!-- /.content-wrapper -->
 <?= $this->endSection() ?>
